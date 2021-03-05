869b7a6a2b4ce01f4bf1314ae7b4acaf23a57160
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-04 19:54:29 -0500

    added reset button

--- JS/loginpage.js
+++ JS/loginpage.js
@@ -1,15 +1,33 @@
-function validateForm() {
-    var username = document.getElementById("uname").value;
-    var password = document.getElementById("pass").value;
+function login()
+ {
+    var uname = document.getElementById("uname").value;
+    var pwd = document.getElementById("pass").value;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     
-    if (username == null || username == "") {
+    if (uname == null || username == '')
+     {
         alert("Please enter the username.");
-        return false;
-    }
-    if (password == null || password == "") {
+     }
+    else if (pass == null || password == '')
+     {
         alert("Please enter the password.");
-        return false;
+     }
+    else if(!filter.test(uname))
+	{
+		alert("Enter valid email id.");
+	}
+    else if(pass.length<6 || pass.length>6)
+    {
+        alert("password length must be 6-8 characters");
     }
-    alert('Login successful');
+    else
+    {
+        alert("Enter password to login");
+    }
 
-  }
+}
+
+function reset(){
+    document.getElementById("uname").value="";
+		document.getElementById("pass").value="";
+}
