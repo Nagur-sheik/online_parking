ce5dc6c60fa804dba1db943bb3c9b6469a2f955d
vinaykumar785557 <77814463+vinaykumar785557@users.noreply.github.com>
2021-03-24 20:35:24 -0400

    js validation for login

--- JS/loginpage.js
+++ JS/loginpage.js
@@ -1,33 +1,65 @@
-function login()
- {
-    var uname = document.getElementById("uname").value;
-    var pwd = document.getElementById("pass").value;
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	//Validation Code For Inputs
+
+    var email = document.getElementById("usname");
+    var password = document.getElementById("pass");
     
-    if (uname == null || username == '')
-     {
-        alert("Please enter the username.");
-     }
-    else if (pass == null || password == '')
-     {
-        alert("Please enter the password.");
-     }
-    else if(!filter.test(uname))
-	{
-		alert("Enter valid email id.");
-	}
-    else if(pass.length<6 || pass.length>6)
-    {
-        alert("password length must be 6-8 characters");
+    
+    var email_error = document.getElementById("email_error");
+    var pass_error = document.getElementById("pass_error");
+    
+    email.addEventListener('textInput', email_Verify);
+    password.addEventListener('textInput', pass_Verify);  
+    
+    function validated() {
+        var emailValue = email.value.trim();
+        var passwordValue = password.value.trim();
+        var atpos = email.value.indexOf("@");
+        var dotpos = email.value.lastIndexOf(".");
+        if (emailValue === '') {
+            email.style.border = "1px solid red";
+            email_error.style.display = "block";
+            email.focus();
+            return false;
+        }
+        if (passwordValue === '') {
+            password.style.border = "1px solid red";
+            pass_error.style.display = "block";
+            password.focus();
+            return false;
+        } 
+        if (atpos < 1 || ( dotpos - atpos < 2 ) ){
+            email.style.border = "1px solid red";
+            email_error.style.display = "block";
+            email.focus();
+            return false;
+        }
+        if (emailValue.length < 9) {
+            email.style.border = "1px solid red";
+            email_error.style.display = "block";
+            email.focus();
+            return false;
+        }
+        if (passwordValue.length < 6) {
+            password.style.border = "1px solid red";
+            pass_error.style.display = "block";
+            password.focus();
+            return false;
+        }
+    
     }
-    else
-    {
-        alert("enter password to login");
+    function email_Verify(){
+        if (email.value.length >= 8) {
+            email.style.border = "1px solid #485e74";
+            email_error.style.display = "none";
+            return true;
+        }
     }
-
-}
-
-function reset(){
-    document.getElementById("uname").value="";
-		document.getElementById("pass").value="";
-}
+    function pass_Verify(){
+        if (password.value.length >= 5) {
+            password.style.border = "1px solid #485e74";
+            pass_error.style.display = "none";
+            return true;
+        }
+    }
+    
+    
